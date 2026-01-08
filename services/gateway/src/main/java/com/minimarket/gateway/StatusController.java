96fa0e677109cdbf493077e6967f29a0c127dfde
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-01-08 15:15:14 +0100

    first routing

--- services/gateway/src/main/java/com/minimarket/gateway/StatusController.java
+++ services/gateway/src/main/java/com/minimarket/gateway/StatusController.java
@@ -3,6 +3,7 @@ package com.minimarket.gateway;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,6 +15,8 @@ public class StatusController {
         Map status = new HashMap();
         status.put("code", 200);
         status.put("message", "OK");
+        status.put("timestamp", LocalDateTime.now().toString());
+        status.put("application", "gateway");
         return status;
     }
 }
