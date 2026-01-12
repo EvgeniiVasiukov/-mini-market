a21dfb5c73441587a0c6260eed34d87fb7504f3c
Evgenii <evgenii@Evgeniis-MacBook-Pro.local>
2026-01-12 14:32:47 +0100

    Postgress added, DB dependencies in catalog pom.xml, tests are skipped in CI for now

--- services/catalog-service/src/main/java/com/minimarket/catalogservice/CatalogController.java
+++ services/catalog-service/src/main/java/com/minimarket/catalogservice/CatalogController.java
@@ -1,8 +1,8 @@
 package com.minimarket.catalogservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.Map;
 
 @RestController
@@ -10,6 +10,10 @@ public class CatalogController {
     @GetMapping("/api/v1/catalog/items")
     public Map<String, String> getCatalogItems() {
         return Map.of("items", "item1", "item2", "item3");
+    }
+    @GetMapping("/api/v1/catalog/items/{id}")
+    public Map<String, String> getCatalogItem(@PathVariable("id") String id) {
+        return Map.of("item", id);
     }
 
 }
